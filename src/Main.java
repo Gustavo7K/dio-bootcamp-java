bab8d164df9bf0b060d9b540e510e47ad5eaaf3a
Gustavo <gustavosales0704@gmail.com>
2024-09-13 04:17:58 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Exercicios {
     public static void main(String[] args) {
         String primeiroNome = "Gleyson";
