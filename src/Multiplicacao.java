47ebae564774c4716447f37505aab6cb1e619ab7
Gustavo <gustavosales0704@gmail.com>
2024-10-09 21:20:37 -0300

    Commit 9/10/2024

---
+++ src/Multiplicacao.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Multiplicacao {
+    public static void main(String[] args) {
+        System.out.println("Essa é uma calculadora simples de multiplicações atente-se as informações abaixo: \n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o numero a ser multiplicado: ");
+        int numeroMultiplicado = scanner.nextInt();
+        System.out.println("Agora o numero multiplicador: ");
+        int multiplicador = scanner.nextInt();
+
+        int resultado = numeroMultiplicado * multiplicador;
+        System.out.println(numeroMultiplicado+" x "+multiplicador+" = "+resultado);
+    }
+}
+
+//código teste de multiplicação infinita entre dois numeros aleatorios gerados pelo codigo*/
+    /*while (numeroMultiplicado() < 11){
+            int numero = numeroMultiplicado();
+            int resultado = numeroMultiplicado() * multiplicador();
+            System.out.println(numero+" X "+multiplicador()+" = "+resultado );
+        }
+    }
+    static int numeroMultiplicado(){
+        return ThreadLocalRandom.current().nextInt(1,11);
+    }
+    static int multiplicador(){
+        return ThreadLocalRandom.current().nextInt(1,11);
+    } //multiplicação infinita entre dois numeros aleatorios gerados pelo codigo*/
